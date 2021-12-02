fee1ab35b7453880bd3f3ba7581e63e869a35d88
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2021-12-02 12:40:16 +0100

    Add day_2.rs part2

--- src/day_2.rs
+++ src/day_2.rs
@@ -16,7 +16,8 @@ pub fn run() {
             )
         })
         .collect::<Vec<(&str, u8)>>();
-    part1(&input)
+    part1(&input);
+    part2(&input);
 }
 
 pub fn part1(input: &Vec<(&str, u8)>) {
@@ -36,6 +37,27 @@ pub fn part1(input: &Vec<(&str, u8)>) {
             }
         }
     }
-    println!("X_POS: {} Y_POS: {}", x_pos, y_pos);
-    println!("Puzzle_Solution: {}", x_pos * y_pos)
+    println!("Part 1: {}", x_pos * y_pos)
+}
+
+pub fn part2(input: &Vec<(&str, u8)>) {
+    let mut x_pos: u32 = 0;
+    let mut y_pos: u32 = 0;
+    let mut aim: u32 = 0;
+    for index in 0..input.len() {
+        match input[index].0 {
+            "forward" => {
+                x_pos += input[index].1 as u32;
+                y_pos += aim * input[index].1 as u32;
+            }
+            "down" => {
+                aim += input[index].1 as u32;
+            }
+            "up" => aim -= input[index].1 as u32,
+            _ => {
+                println!("Error, invalid instruction!")
+            }
+        }
+    }
+    println!("Part 2: {}", x_pos * y_pos)
 }
