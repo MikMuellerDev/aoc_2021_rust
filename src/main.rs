a24f1d1f5ce91bb300fa0efb0d3a522c9c8373a5
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2021-12-08 14:09:03 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,12 @@ mod day_7;
 fn main() {
     let start = time::Instant::now();
 
-    // day_1::run();
-    // day_2::run();
-    // day_3::run();
+    day_1::run();
+    day_2::run();
+    day_3::run();
     day_4::run();
-    // day_5::run();
-    // day_6::run();
-    // day_7::run();
+    day_5::run();
+    day_6::run();
+    day_7::run();
     println!("\n<------------->\n Execution took {:?}", start.elapsed())
 }
